3b0f8829fa6a04c41e63b44ed2adb818dd8d61a7
GABI <gabriellimrqs1998@gmail.com>
2023-04-19 15:47:37 -0300

    UseAuth

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactNode, createContext } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
     user: UserDTO;
+    signIn: (email: string, password: string) => void;
 }
-//Nesse contexto vamos compartilhar coisas relacionadas a autenticação do usuário.
-
 
 type AuthContextProviderProps = {
     children: ReactNode;
 }
 
-
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+
 
 export function AuthContextProvider({ children } : AuthContextProviderProps) {
 
+    const[user, setUser] = useState({
+        id: '1',
+        name: 'teste',
+        email:'teste@gmail.com',
+        avatar:'teste.png',
+    });
+    //vamos criar uma estado para guardar as informações do usuário, o estado muda todos os lugares que estiverem usando ele.
+
+
+    function signIn(email:string, password:string){
+        setUser({
+            id: '',
+            name:'',
+            email,
+            avatar: '',
+          }) 
+    }
+    //podemos deixar a logica que vai atualizar(useState) nosso contexto aqui no AuthContext
+
+
+    //Ao invés de compartilharmos o user e setUser, passamos o signIn porque criamos uma função somente para passar esses dados.
     return (
-        <AuthContext.Provider value={{
-            user: {
-                id: '1',
-                name: 'teste',
-                email:'teste@gmail.com',
-                avatar:'teste.png',
-            }
-        }}>
+        <AuthContext.Provider value={{ user, signIn }}>
         { children }
         </AuthContext.Provider>
     )
